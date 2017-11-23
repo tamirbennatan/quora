a2167a2dab4444f453e33dacd6babcc93ade342d
Tamir Bennatan <timibennatan@gmail.com>
2017-11-23 17:39:22 -0500

    updates on distance metrics

--- human_responses/human_accuracy.R
+++ human_responses/human_accuracy.R
@@ -15,7 +15,11 @@ responses = responses %>% inner_join(
 
 # Get the human response accuracy
 responses = responses %>% 
-      mutate(human.correct = human.response == is_duplicate)
+      mutate(human.correct = human.response == is_duplicate, 
+	group = floor(row_number()/40))
       
 human.accuracy = mean(responses$human.correct)
 print(paste("Human Accuracy on ", nrow(responses), " responses: ", human.accuracy, sep = ""))
+
+responses %>% group_by(group) %>%
+	summarize(acc = mean(human.correct))
